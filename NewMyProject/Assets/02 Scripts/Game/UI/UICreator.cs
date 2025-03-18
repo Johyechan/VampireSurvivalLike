11b48f6b8a091e06867c7cf391170a54208e588f
Johyechan <johyechan6@gmail.com>
2025-03-18 10:02:32 +0900

    Refactoring Project
    1. Developing System that Create UI

---
+++ NewMyProject/Assets/02 Scripts/Game/UI/UICreator.cs
@@ -0,0 +1,14 @@
+using MyUtil.Pool;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyUI
+{
+    public class UICreator : MonoBehaviour
+    {
+        public void CreateUI(List<ObjectPoolType> types, Transform parentTrans, int x, int y, int width, int height, float spacing, int[,] backpackArr = null)
+        {
+
+        }
+    }
+}
